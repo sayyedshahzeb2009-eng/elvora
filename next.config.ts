c44376d050d99d98012fa3bafd436c2f27f30e67
sayyedshahzeb2009-eng <sayyedshahzeb2009@gmail.com>
2026-08-30 14:24:11 +0530

    chore: remove Next.js files from Shopify theme branch

--- next.config.ts
+++
@@ -1,1 +0,0 @@
-import type {NextConfig} from 'next';const nextConfig:NextConfig={images:{remotePatterns:[{protocol:'https',hostname:'images.unsplash.com'}]},reactStrictMode:true};export default nextConfig;
